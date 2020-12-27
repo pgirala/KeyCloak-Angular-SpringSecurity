b68957aabd0fd80c2f89d770aeb75284616f8ef3
pgirala <pgirala@gmail.com>
2020-12-27 09:41:50 +0100

    Incorpora ACL.

--- src/main/java/com/pj/keycloak/service/EmployeeService.java
+++ src/main/java/com/pj/keycloak/service/EmployeeService.java
@@ -5,15 +5,24 @@ import com.pj.keycloak.model.Employee;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 public interface EmployeeService
 {
+    @PostFilter("hasPermission(filterObject, 'READ')")
     List<Employee> findAll();
 
+    @PostAuthorize("hasPermission(returnObject, 'READ')")
     Optional<Employee> findById(Long id);
 
+    @PreAuthorize("hasPermission(#employee, 'WRITE')")
     Employee updateProfile(Employee employee);
 
+    @PreAuthorize("hasPermission(#employee, 'CREATE')")
     void saveAndFlush(Employee employee);
 
+    @PreAuthorize("hasPermission(#employee, 'DELETE')")
     void deleteById(Long id);
 }

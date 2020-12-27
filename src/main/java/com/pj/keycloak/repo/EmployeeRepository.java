b186afda4736c97758360ceb427c9c47f4ee4275
pgirala <pgirala@gmail.com>
2020-12-27 23:45:51 +0100

    Primera versión con Javers integrado.

--- src/main/java/com/pj/keycloak/repo/EmployeeRepository.java
+++ src/main/java/com/pj/keycloak/repo/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.pj.keycloak.repo;
 import com.pj.keycloak.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.javers.spring.annotation.JaversSpringDataAuditable;
+
+@JaversSpringDataAuditable
 public interface EmployeeRepository extends JpaRepository<Employee,Long>
 {
 }

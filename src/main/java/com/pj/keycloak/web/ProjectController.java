28450612c111293b17c5fa260ed1163380cdc43b
pgirala <pgirala@gmail.com>
2021-01-27 23:55:27 +0100

    Graba correctamente el proyecto pero...

--- src/main/java/com/pj/keycloak/web/ProjectController.java
+++ src/main/java/com/pj/keycloak/web/ProjectController.java
@@ -14,54 +14,43 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("/api/v1/project")
-public class ProjectController
-{
+public class ProjectController {
     private final ProjectService projectService;
     private final UserInfoUtil userInfoUtil;
 
-    private Logger logger= LoggerFactory.getLogger(ProjectController.class);
+    private Logger logger = LoggerFactory.getLogger(ProjectController.class);
 
-
-    public ProjectController(ProjectService projectService, UserInfoUtil userInfoUtil)
-    {
+    public ProjectController(ProjectService projectService, UserInfoUtil userInfoUtil) {
         this.projectService = projectService;
         this.userInfoUtil = userInfoUtil;
     }
 
     @GetMapping(path = "/list")
-    public List<Project> findAll(HttpServletRequest httpServletRequest)
-    {
-        logger.info("User Id: {}",userInfoUtil.getPreferredUsername(httpServletRequest));
+    public List<Project> findAll(HttpServletRequest httpServletRequest) {
+        logger.info("User Id: {}", userInfoUtil.getPreferredUsername(httpServletRequest));
         return projectService.findAll();
     }
 
     @GetMapping(path = "/find/{id}")
-    public Optional<Project> findById(@PathVariable Long id)
-    {
+    public Optional<Project> findById(@PathVariable Long id) {
         return projectService.findById(id);
     }
 
     @PostMapping(path = "/update")
-    public Project update(@RequestBody Project project)
-    {
+    public Project update(@RequestBody Project project) {
         return projectService.updateProfile(project);
     }
 
     @PostMapping(path = "/create")
-    public List<Project> create(@RequestBody Project project)
-    {
+    public List<Project> create(@RequestBody Project project) {
         project.setId(Project.getIdClase()); // para aplicar el permiso CREATE en la ACL
-        project.setName("(Nombre del proyecto)");
-        project.setLocation("(Ubicación)");
-        project.setBudget(0.0);
         projectService.saveAndFlush(project, userInfoUtil);
 
         return projectService.findAll();
     }
 
     @DeleteMapping(path = "/delete/{id}")
-    public void deleteProject(@PathVariable Long id)
-    {
+    public void deleteProject(@PathVariable Long id) {
         projectService.deleteById(id);
     }
 
